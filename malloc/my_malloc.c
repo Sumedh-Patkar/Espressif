41409fc63618bbbf0eb734cc0fc30b60c6f71b9b
Sumedh <sumedh@debian.sumedh>
2018-09-01 14:15:37 +0530

    updated code

--- malloc/my_malloc.c
+++ malloc/my_malloc.c
@@ -11,20 +11,20 @@ struct header
 	char alloc_status;			//'a' if allocated, 'f' if free
 }s;
 
-int maxsize = sizeof(buffer) - 2*sizeof(struct header); //initial available size
+int maxsize = sizeof(buffer) - 2*sizeof(struct header);   //currently available maximum block size
 
 char* my_malloc(int n)
 {
-  if(n > maxsize)				//enough space is not available
-  	return 0;
+  if(n > maxsize)						//enough space is not available
+  	return NULL;
   else
   {
-    memcpy(&s,gptr,sizeof(s));
-  	char *lptr = gptr;			//iterator to find best fit block
-  	int minsize = s.size;
-  	char *minsizeptr = gptr;	//points to best fit block
+    memcpy(&s,gptr,sizeof(s));			//copy header of free location to s
+  	char *lptr = gptr;			      	//iterator to find best fit block
+  	int minsize = sizeof(buffer);
+  	char *minsizeptr = gptr;	    	//points to best fit block
 
-    struct header temp;       //temporary structure object
+    struct header temp;           		//temporary structure object
 
     do {
       /*after this loop,
@@ -33,34 +33,23 @@ char* my_malloc(int n)
       */
       memcpy(&temp,lptr,sizeof(struct header));
 
-      if((temp.size > n + 2*sizeof(struct header)) && (temp.size < minsize))
+      if((temp.size >= n + 2*sizeof(struct header)) && (temp.size < minsize))
       {
         minsize = temp.size;
         minsizeptr = lptr;
       }
-
+      // printf("PRINTING TEMP->NEXT = %d\n",temp.next);
+      // if(temp.next == 0)
+        // printf("LPTR = %d\n",lptr);
       lptr = temp.next;
     } while(lptr != gptr);
+    memcpy(&temp,minsizeptr,sizeof(struct header));
 
     /*Make the next header*/
     lptr = (minsizeptr + n + sizeof(struct header));   //lptr now points to the remaning part of the free location
-    // if(lptr > buffer + sizeof(buffer))
-    // {
-    //   /*
-    //   point to the first free location
-    //   from the beginning
-    //   */
-    //   lptr = buffer;
-    //   struct header temp2;
-    //   memcpy(&temp2,lptr,sizeof(struct header));
-    //   while(temp2.alloc_status == 'a')
-    //   {
-    //     lptr = lptr + temp2.size + sizeof(struct header);
-    //     memcpy(&temp2,lptr,sizeof(struct header));
-    //   }
-    // }
+    gptr = lptr;                                      //update GPTR
 
-    //if it was the ONLY existing free block, then it will point to itself.
+    /*if it was the ONLY existing free block, then it will point to itself*/
     if(temp.next == minsizeptr)
         s.next = lptr;
     else
@@ -73,7 +62,7 @@ char* my_malloc(int n)
     /*Update Max Size*/
     if(temp.size == maxsize)
     {
-      maxsize = maxsize - n - sizeof(struct header);
+      maxsize = maxsize - n - 2*sizeof(struct header);
       lptr = temp.next;
       do
       {
@@ -96,24 +85,15 @@ char* my_malloc(int n)
 
 void my_free(char *x)
 {
-  gptr = x;
+  gptr = x - sizeof(struct header);
   struct header temp;
-  char *iteratePtr;
+  // char *iteratePtr;
   memcpy(&s,gptr,sizeof(struct header));        //s now points to the block being freed
   s.alloc_status = 'f';                         //mark current block as 'free'
 
   char *lptr = gptr;
-  lptr=lptr + s.size;
-  char *lptr2;
-
+  lptr=lptr + s.size + sizeof(struct header);
   memcpy(&temp,lptr,sizeof(struct header));
-
-  // for merging below consecutive block
-  if(temp.alloc_status == 'f')                  //check if next immediate block is free, in order to merge
-  {
-    s.size = s.size + temp.size + sizeof(struct header);
-    s.next = temp.next;
-  }
 
   //this loop finds the first free block after x
   while(temp.alloc_status != 'f')
@@ -123,22 +103,40 @@ void my_free(char *x)
   }
   //now lptr points to the first free block
 
-  lptr2 = lptr;
-  memcpy(&temp,lptr2,sizeof(struct header))
+  char *lptr2 = lptr;
+  memcpy(&temp,lptr2,sizeof(struct header));
 
   while(temp.next != lptr)
   {
+    printf("%d\n",lptr2);
+    if(temp.next == NULL)
+    {
+      printf("FAIL\n");
+
+    }
     lptr2 = temp.next;
     memcpy(&temp,lptr2,sizeof(struct header));
   }
   temp.next = gptr;
+  memcpy(lptr2,&temp,sizeof(struct header));
   s.next = lptr;
+  memcpy(gptr,&s,sizeof(struct header));
 
-  if(lptr2 + temp.size + sizeof(struct header) == gptr)                  //
+  //if LOWER block and block to be freed are consecutive, merge them
+  if(gptr + s.size + sizeof(struct header) == lptr)           //check if next immediate block is free, in order to merge
+  {
+    s.size = s.size + temp.size + sizeof(struct header);
+    s.next = temp.next;
+    memcpy(gptr,&s,sizeof(struct header));
+  }
+
+  //if upper block and block to be freed are consecutive, merge them
+  if(lptr2 + temp.size + sizeof(struct header) == gptr)
   {
     temp.size = temp.size + s.size + sizeof(struct header);
     temp.next = s.next;
     gptr = lptr2;
+    memcpy(gptr,&temp,sizeof(struct header));
   }
 
 }
@@ -150,21 +148,27 @@ int main()
   s.alloc_status = 'f';
   memcpy(gptr,&s,sizeof(s));
 
-  // char *p = my_malloc(4000);
-  // printf("%d\n",p);
-  // char *q = my_malloc(1000);
-  // printf("%d\n",q);
-  // char *r = my_malloc(3000);
-  // printf("%d\n",r);
-  // my_free(r);
-  // char *s = my_malloc(2000);
-  // printf("%d\n",s);
+  printf("GPTR = %d\n",gptr);
+  char *p = my_malloc(4000);
+  printf("P = %d\n",p);
+  printf("GPTR = %d\n",gptr);
+  char *q = my_malloc(1000);
+  printf("Q = %d\n",q);
+  printf("GPTR = %d\n",gptr);
+  char *r = my_malloc(3000);
+  printf("R = %d\n",r);
+  printf("GPTR = %d\n",gptr);
+  my_free(q);
+  printf("GPTR = %d\n",gptr);
+  char *s = my_malloc(2000);
+  printf("S = %d\n",s);
+  printf("GPTR = %d\n",gptr);
   // char *t = my_malloc(2000);
   // printf("%d\n",t);
-  // my_free(t);
   // my_free(s);
-  // my_free(q);
+  // my_free(t);
   // my_free(p);
-
+  my_free(r);
+  printf("GPTR = %d\n",gptr);
   return 0;
 }
